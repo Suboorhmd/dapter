6f7e5bb1094930b9e3ad434030ae7d9d5b13031f
Suboorhmd <55682820+Suboorhmd@users.noreply.github.com>
2022-02-28 01:16:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -238,8 +238,4 @@ def upload_file():
 
 
 if __name__=="__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-p','--port',type=int,default=5000, help="Running port")
-    parser.add_argument("-H","--host",type=str,default='8080', help="Address to broadcast")
-    args = parser.parse_args()
-    app.run(host=args.host,port=args.port)
+    app.run(port=8080)
